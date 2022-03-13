faffe71a0ab8ac1288b41e4b5d2670f248030542
Nguyen Duyen <s2200128@BC-C02GR0BVQ6LR-MacBook-Air.local>
2022-03-13 17:38:47 +0200

    failed to add modules to main.js

--- src/main.js
+++ src/main.js
@@ -41,6 +41,51 @@
 // API.get("duyennguyen123").then((data) => console.log(data));
 // API.post("duyennguyen123").then((data) => console.log(data));
 
+// IMPORT MODULES
+import snackbar from "snackbar";
+import "snackbar/dist/snackbar.min.css";
+import Chart from "chart.js/auto";
+
+// CHARTS
+
+const ctx = document.getElementById("myChart");
+const myChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
 class FoodObject {
   constructor(name, carb, protein, fat) {
     this.name = name;
@@ -94,3 +139,19 @@ form.addEventListener("submit", (e) => {
   addCard();
   form.reset();
 });
+
+(async () => {
+  const rawResponse = await fetch(
+    "https://firestore.googleapis.com/v1/projects/programmingjs-90a13/databases/(default)/documents/",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ a: 1 }),
+    }
+  );
+  const content = await rawResponse.json();
+  console.log(content);
+})();
