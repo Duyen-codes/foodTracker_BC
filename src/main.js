1b81cdd7a688a3e09927c0a6877400861e68c6ed
Nguyen Duyen <s2200128@BC-C02GR0BVQ6LR-MacBook-Air.local>
2022-03-14 06:40:19 +0200

    get inputs and displayed inputs on page on form submit

--- src/main.js
+++ src/main.js
@@ -1,90 +1,12 @@
-// class FetchWrapper {
-//   constructor(baseURL) {
-//     this.baseURL = baseURL;
-//   }
-
-//   get(endpoint) {
-//     return fetch(this.baseURL + endpoint).then((response) => response.json());
-//   }
-
-//   put(endpoint, body) {
-//     return this._send("put", endpoint, body);
-//   }
-
-//   post(endpoint, body) {
-//     return this._send("post", endpoint, body);
-//   }
-
-//   patch(endpoint, body) {
-//     return this._send("patch", endpoint, body);
-//   }
-
-//   delete(endpoint, body) {
-//     return this._send("delete", endpoint, body);
-//   }
-
-//   _send(method, endpoint, body) {
-//     return fetch(this.baseURL + endpoint, {
-//       method,
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(body),
-//     }).then((response) => response.json());
-//   }
-// }
-
-// const API = new FetchWrapper(
-//   "https://firestore.googleapis.com/v1/projects/programmingjs-90a13/databases/(default)/documents/"
-// );
-
-// API.get("duyennguyen123").then((data) => console.log(data));
-// API.post("duyennguyen123").then((data) => console.log(data));
-
 // IMPORT MODULES
-import snackbar from "snackbar";
-import "snackbar/dist/snackbar.min.css";
+// import snackbar from "snackbar";
+// import "snackbar/dist/snackbar.min.css";
 import Chart from "chart.js/auto";
+import { FetchWrapper } from "./fetch-wrapper.js";
 
-// CHARTS
-
-const ctx = document.getElementById("myChart");
-const myChart = new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
-  },
-});
+const API = new FetchWrapper(
+  "https://firestore.googleapis.com/v1/projects/programmingjs-90a13/databases/(default)/documents/"
+);
 
 class FoodObject {
   constructor(name, carb, protein, fat) {
@@ -96,7 +18,6 @@ class FoodObject {
 }
 
 const cards = document.querySelector(".cards");
-const addBtn = document.querySelector(".add-btn");
 const logAmount = document.querySelector(".log-amount");
 const carbAmount = document.querySelector(".carb-amount");
 const proteinAmount = document.querySelector(".protein-amount");
@@ -104,54 +25,58 @@ const fatAmount = document.querySelector(".fat-amount");
 const cardName = document.querySelector(".card-name");
 const form = document.querySelector("form");
 
-form.addEventListener("submit", (e) => {
+const getInputs = (e) => {
   e.preventDefault();
+  console.log("submit");
   const nameInput = document.querySelector("#food-names").value;
   const carbInput = Number(document.querySelector("#carbs").value);
   const proteinInput = Number(document.querySelector("#protein").value);
   const fatInput = Number(document.querySelector("#fat").value);
 
   let foodItem = new FoodObject(nameInput, carbInput, proteinInput, fatInput);
+  foodItem = { nameInput, carbInput, proteinInput, fatInput };
   console.log(foodItem);
   logAmount.textContent = carbInput + proteinInput + fatInput + "g";
 
-  const addCard = (foodItem) => {
-    cards.innerHTML += `
-    <div class="card-item">
-    <h3 class="card-name">${nameInput}</h3>
-    <p><span>${carbInput + proteinInput + fatInput}g</span> calories</p>
-    <ul class="nutrition-details">
-      <li>
-        <p>Carbs</p>
-        <p class="carb-amount">${carbInput}g</p>
-      </li>
-      <li>
-        <p>Protein</p>
-        <p class="protein-amount">${proteinInput}g</p>
-      </li>
-      <li>
-        <p>Fat</p>
-        <p class="fat-amount">${fatInput}g</p>
-      </li>
-    </ul>
-  </div>`;
-  };
-  addCard();
-  form.reset();
-});
+  renderCard(foodItem);
+};
 
-(async () => {
-  const rawResponse = await fetch(
-    "https://firestore.googleapis.com/v1/projects/programmingjs-90a13/databases/(default)/documents/",
-    {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify({ a: 1 }),
-    }
-  );
-  const content = await rawResponse.json();
-  console.log(content);
-})();
+form.addEventListener("submit", getInputs);
+
+const renderCard = ({ nameInput, carbInput, proteinInput, fatInput }) => {
+  cards.innerHTML += `
+  <div class="card-item">
+  <h3 class="card-name">${nameInput}</h3>
+  <p><span>${carbInput + proteinInput + fatInput}g</span> calories</p>
+  <ul class="nutrition-details">
+    <li>
+      <p>Carbs</p>
+      <p class="carb-amount">${carbInput}g</p>
+    </li>
+    <li>
+      <p>Protein</p>
+      <p class="protein-amount">${proteinInput}g</p>
+    </li>
+    <li>
+      <p>Fat</p>
+      <p class="fat-amount">${fatInput}g</p>
+    </li>
+  </ul>
+</div>`;
+  form.reset();
+};
+// (async () => {
+//   const rawResponse = await fetch(
+//     "https://firestore.googleapis.com/v1/projects/programmingjs-90a13/databases/(default)/documents/",
+//     {
+//       method: "POST",
+//       headers: {
+//         Accept: "application/json",
+//         "Content-type": "application/json",
+//       },
+//       body: JSON.stringify({ a: 1 }),
+//     }
+//   );
+//   const content = await rawResponse.json();
+//   console.log(content);
+// })();
